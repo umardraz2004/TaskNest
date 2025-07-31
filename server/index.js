78d83cc5757042b1d456037e9ac6695ec08d0ee6
umardraz2004 <umardraz6965@gmail.com>
2025-08-01 03:53:32 +0500

    some changes

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ connectDB();
 // Middleware to parse JSON
 app.use(express.json());
 const corsOptions = {
-  origin: "https://task-nest-three-xi.vercel.app/",
+  origin: "https://task-nest-three-xi.vercel.app",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
